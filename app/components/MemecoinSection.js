24652bc67e257331ed8a75205ccd52d9dfc0f2a5
shubhanker <ankursaxena347@gmail.com>
2025-01-31 00:20:32 +0530

    wip:changes-done

--- app/components/MemecoinSection.js
+++ app/components/MemecoinSection.js
@@ -13,7 +13,7 @@ export default function MemecoinSection() {
           <div className="grid grid-cols-1 md:grid-cols-2 gap- md:gap-20">
             <div className="flex flex-col space-y-4 md:space-y-9">
               <h2 className="text-start text-2xl md:text-center md:text-4xl">Fair Launch</h2>
-              <h2 className="text-start text-4xl md:text-5xl">NO INVESTORS.</h2>
+              <h2 className="text-start text-4xl md:text-5xl">NO INSIDERS.</h2>
               <h2 className="text-start text-4xl md:text-5xl">NO LOCKUPS.</h2>
               <h2 className="text-start text-4xl md:text-5xl">NO CABAL.</h2>
               <h2 className="text-start text-4xl md:text-5xl">NO KOL.</h2>
